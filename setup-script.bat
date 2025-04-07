91ef7078c395a16a7edf12fc80e9d2e9c11fa552
Silviu Winter <si2lv3l@gmail.com>
2025-04-07 13:35:11 +0200

    updated

--- setup-script.bat
+++ setup-script.bat
@@ -5,7 +5,7 @@ git --version >nul 2>&1
 if %ERRORLEVEL% NEQ 0 (
     echo Git is not installed. Installing...
     powershell -Command "Invoke-WebRequest -Uri https://github.com/git-for-windows/git/releases/latest/download/Git-2.42.0-64-bit.exe -OutFile %USERPROFILE%\git-installer.exe"
-    powershell -Command "Start-Process %USERPROFILE%\git-installer.exe -ArgumentList '/silent' -Wait"
+    powershell -Command "Start-Process %USERPROFILE%\git-installer.exe -ArgumentList '/silent' -Verb RunAs -Wait"
     echo Git installed successfully.
 ) else (
     echo Git is already installed.
@@ -17,7 +17,8 @@ cd project
 
 REM Install Docker for Windows
 echo Installing Docker for Windows...
-powershell -Command "Start-Process 'https://desktop.docker.com/win/stable/Docker%20Desktop%20Installer.exe' -Wait"
+powershell -Command "Invoke-WebRequest -Uri https://desktop.docker.com/win/stable/Docker%20Desktop%20Installer.exe -OutFile %USERPROFILE%\docker-installer.exe"
+powershell -Command "Start-Process %USERPROFILE%\docker-installer.exe -Verb RunAs -Wait"
 
 REM Wait for Docker installation to complete
 echo Please complete the Docker installation and press any key to continue...
